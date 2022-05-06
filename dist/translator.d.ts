9718ffa2de73109e8b7ad5efbe096e37c5fcec70
lacepek <la.cepek@gmail.com>
2022-05-06 12:28:22 +0200

    custom interpolation

--- dist/translator.d.ts
+++ dist/translator.d.ts
@@ -1,5 +1,12 @@
+export declare type Options = {
+    interpolation?: Interpolation;
+};
+export declare type Interpolation = {
+    prefix?: string;
+    suffix?: string;
+};
 export declare function translate(key: string, locale: string, translations: {
     [key: string]: any;
 }, params?: {
     [key: string]: any;
-}): any;
+}, options?: Options): any;
--- src/translator.ts
+++ src/translator.ts
@@ -1,48 +1,61 @@
+export type Options = {
+    interpolation?: Interpolation;
+};
+
+export type Interpolation = {
+    prefix?: string;
+    suffix?: string;
+};
+
 export function translate(
-  key: string,
-  locale: string,
-  translations: { [key: string]: any },
-  params?: { [key: string]: any }
+    key: string,
+    locale: string,
+    translations: { [key: string]: any },
+    params?: { [key: string]: any },
+    options?: Options
 ) {
-  if (key === null || key === undefined) {
-    return null;
-  }
-
-  const keys = key.split('.');
-  const translation = keys.reduce(searchForKey, translations[locale]);
-
-  if (!translation) {
-    return key;
-  }
-
-  if (params) {
-    if (translation instanceof Function) {
-      return translation(params);
+    if (key === null || key === undefined) {
+        return null;
     }
 
-    return injectParamsToTranslation(params, translation);
-  }
+    const keys = key.split(".");
+    const translation = keys.reduce(searchForKey, translations[locale]);
 
-  return translation;
+    if (!translation) {
+        return key;
+    }
+
+    if (params) {
+        if (translation instanceof Function) {
+            return translation(params);
+        }
+
+        return injectParamsToTranslation(params, translation, options?.interpolation);
+    }
+
+    return translation;
 }
 
 function searchForKey(current: string, key: string) {
-  if (!current || !current[key]) {
-    return null;
-  }
-
-  return current[key];
-}
-
-function injectParamsToTranslation(params: { [key: string]: any }, translation: string) {
-  const keys = Object.keys(params);
-  return keys.reduce((accumulator: string, key: string) => {
-    const needle = `{{${key}}}`;
-    if (accumulator.includes(needle)) {
-      const value = params[key];
-      return accumulator.replace(needle, value);
+    if (!current || !current[key]) {
+        return null;
     }
 
-    return accumulator;
-  }, translation);
+    return current[key];
+}
+
+function injectParamsToTranslation(params: { [key: string]: any }, translation: string, interpolation: Interpolation) {
+    const keys = Object.keys(params);
+    return keys.reduce((accumulator: string, key: string) => {
+        const needlePrefix = interpolation?.prefix || "{{"; 
+        const needleSuffix = interpolation?.suffix || "}}"; 
+        const needle = `${needlePrefix}${key}${needleSuffix}`;
+        
+        if (accumulator.includes(needle)) {
+            const value = params[key];
+            return accumulator.replace(needle, value);
+        }
+
+        return accumulator;
+    }, translation);
 }
